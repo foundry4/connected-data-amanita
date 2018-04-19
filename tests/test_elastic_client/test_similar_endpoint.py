99f62fbf5302fd832bd299d8538f228a04cb591e
windet01 <theo.windebank@bbc.co.uk>
2018-04-19 17:11:47 +0100

    more tests for similar endpoint

--- tests/test_elastic_client/test_similar_endpoint.py
+++ tests/test_elastic_client/test_similar_endpoint.py
@@ -12,6 +12,8 @@ from app.clients.elastic.querybuilder.get_similar import build_query_body
 from exceptions.queryexceptions import InvalidInputParameterCombination
 
 item_uri = 'programmes:bbc.co.uk,2018/FIXME/p05q11tt'
+
+
 def test_response_processing(flask_app, monkeypatch):
     # dont test parameters here just test content processing
     monkeypatch.setattr(api, 'DB_CLIENT', ESClient)  # instead of using env vars to select db
@@ -29,46 +31,33 @@ def test_response_processing(flask_app, monkeypatch):
 # query building
 def test_query_building_no_params():
     body = build_query_body(item_uri=item_uri)
-    assert body == {'query': {'bool': {'should': [{'more_like_this': {'like': {'_index': 'pips', '_type': 'clip', '_id': 'programmes:bbc.co.uk,2018/FIXME/p05q11tt'}, 'fields': ['title', 'masterBrand.mid', 'mediaType'], 'min_term_freq': 1, 'min_doc_freq': 1}}, {'nested': {'path': 'genres', 'query': {'more_like_this': {'fields': ['genres.key'], 'like': {'_index': 'pips', '_type': 'clip', '_id': 'programmes:bbc.co.uk,2018/FIXME/p05q11tt'}, 'min_term_freq': 1, 'min_doc_freq': 1}}}}]}}, 'from': 0, 'size': 20}
+    assert body == {'query': {'bool': {'should': [{'more_like_this': {
+        'like': {'_index': 'pips', '_type': 'clip', '_id': 'programmes:bbc.co.uk,2018/FIXME/p05q11tt'},
+        'fields': ['title', 'masterBrand.mid', 'mediaType'], 'min_term_freq': 1, 'min_doc_freq': 1}}, {
+        'nested': {'path': 'genres', 'query': {
+            'more_like_this': {'fields': ['genres.key'],
+                               'like': {'_index': 'pips', '_type': 'clip',
+                                        '_id': 'programmes:bbc.co.uk,2018/FIXME/p05q11tt'},
+                               'min_term_freq': 1,
+                               'min_doc_freq': 1}}}}]}}, 'from': 0,
+        'size': 20}
 
 
-# non_implemented_params = ['published_after', 'region', 'similarity_method']
-#
-#
-# @pytest.mark.parametrize('non_implemented_param', non_implemented_params)
-# def test_query_building_params_not_implemented(non_implemented_param):
-#     with pytest.raises(NotImplementedError):
-#         build_query_body(**{non_implemented_param: ''})
-#
-#
-# def test_query_building_params_invalid_combination():
-#     with pytest.raises(InvalidInputParameterCombination):
-#         build_query_body(sort='', random=True)
-#
-#
-# def test_query_building_all_implemented_params():
-#     params = json.load(open("test_elastic_client/data/param_examples.json"))
-#     expected_params = list(inspect.signature(build_query_body).parameters)
-#
-#     val_params = {k: v['validated'] for k, v in params.items() if
-#                   k not in non_implemented_params and k in expected_params}
-#     body = build_query_body(**val_params)
-#     assert body == {
-#         'query': {
-#             'bool': {
-#                 'filter': [
-#                     {'term': {'mediaType': 'audio'}},
-#                     {'term': {'mediaType': 'video'}},
-#                     {'range': {'duration': {'lte': 100}}},
-#                     {'nested': {'path': 'genres', 'query': {'match': {'genres.key': 'music'}}}},
-#                     {'nested': {'path': 'genres', 'query': {'match': {'genres.key': 'comedy'}}}}
-#                 ]
-#             }
-#         },
-#         'sort': ['duration', 'publicationDate', 'masterBrand',
-#                  {'duration': {'order': 'desc'}},
-#                  {'publicationDate': {'order': 'desc'}},
-#                  {'masterBrand': {'order': 'desc'}}],
-#         'from': 10,
-#         'size': 10
-#     }
+non_implemented_params = ['published_after', 'region', 'similarity_method']
+
+
+@pytest.mark.parametrize('non_implemented_param', non_implemented_params)
+def test_query_building_params_not_implemented(non_implemented_param):
+    with pytest.raises(NotImplementedError):
+        build_query_body(**{non_implemented_param: ''})
+
+
+def test_query_building_all_implemented_params():
+    params = json.load(open("test_elastic_client/data/param_examples.json"))
+    expected_params = list(inspect.signature(build_query_body).parameters)
+
+    val_params = {k: v['validated'] for k, v in params.items() if
+                  k not in non_implemented_params and k in expected_params}
+    body = build_query_body(**val_params)
+    assert body == {'query': {'bool': {'filter': [{'term': {'mediaType': 'audio'}}, {'term': {'mediaType': 'video'}}, {'range': {'duration': {'lte': 100}}}], 'should': [{'more_like_this': {'like': {'_index': 'pips', '_type': 'clip', '_id': 'http://exampleuri.com'}, 'fields': ['title', 'masterBrand.mid', 'mediaType'], 'min_term_freq': 1, 'min_doc_freq': 1}}, {'nested': {'path': 'genres', 'query': {'more_like_this': {'fields': ['genres.key'], 'like': {'_index': 'pips', '_type': 'clip', '_id': 'http://exampleuri.com'}, 'min_term_freq': 1, 'min_doc_freq': 1}}}}], 'minimum_should_match': 1}}, 'from': 10, 'size': 10}
+
