84bf0a290782c1fbf12a022e57824314f373b56d
windet01 <theo.windebank@bbc.co.uk>
2018-04-19 10:56:49 +0100

    more tests

--- app/apiparams/types.py
+++ app/apiparams/types.py
@@ -1,4 +1,4 @@
-from dateutil import parser
+import iso8601
 from rdflib import URIRef, Literal
 
 from app.clients.sparql.namespaces import namespaces as NS
@@ -6,6 +6,8 @@ from app.clients.sparql.namespaces import namespaces as NS
 
 class BoolFromString:
     def __init__(self, bool_str):
+        if bool_str not in ['true', 'True', 'false', 'False']:
+            raise ValueError(f'"{bool_str}" is not a valid boolean string.')
         self.bool = True if bool_str in ['true', 'True'] else False
 
     def __bool__(self):
@@ -35,8 +37,11 @@ class LowercaseLiteral:
 
 
 class ValidatedDatetime:
-    def __init__(self, datetime_str):
-        self.datetime = parser.parse(datetime_str)
+    def __init__(self, datetime: str):
+        datetime = datetime[1:] if datetime.startswith('P') else datetime
+        self.datetime_obj = iso8601.parse_date(datetime)
+        self.datetime = datetime
 
     def __str__(self):
-        return str(self.datetime)
+        return self.datetime
+
--- tests/test_parameter_handling.py
+++ tests/test_parameter_handling.py
@@ -1,9 +1,14 @@
+import pytest
+from rdflib import URIRef
+
 from app.apiparams import lists, types, validator
+from app.apiparams.types import BoolFromString, MediaLiteral, LowercaseLiteral, ValidatedDatetime
 
 
 class Definitions:
     def __getattr__(self, item):
         return None
+
 
 # test parameter lists
 def test_correct_content_parameter_list():
@@ -28,5 +33,42 @@ def test_correct_item_parameter_list():
     implemented_content_parameters = lists.get_param_validators_for_endpoint('item', Definitions())
     assert sorted(implemented_content_parameters) == sorted(specced_content_parameters)
 
+
 # test parameter types
-#TODO : HERE
+def test_bool_from_string():
+    bool = BoolFromString('true')
+    assert bool
+    bool = BoolFromString('True')
+    assert bool
+    bool = BoolFromString('false')
+    assert not bool
+    bool = BoolFromString('False')
+    assert not bool
+    with pytest.raises(ValueError):
+        BoolFromString('notbool')
+
+
+def test_media_literal():
+    video = MediaLiteral('video')
+    assert video.n3() == '<http://purl.org/dc/terms/MovingImage>'
+    audio = MediaLiteral('audio')
+    assert audio.n3() == '<http://purl.org/dc/terms/Sound>'
+    with pytest.raises(ValueError):
+        MediaLiteral('notmedia')
+
+
+def test_lowercase_literal():
+    lowercase = LowercaseLiteral('MixedCase')
+    assert lowercase.n3() == '"mixedcase"'
+
+
+def test_validated_datetime():
+    datetime = ValidatedDatetime('19941128T155300')
+    assert str(datetime) == '19941128T155300'
+    datetime_obj = datetime.datetime_obj
+    assert datetime_obj.year == 1994
+    assert datetime_obj.month == 11
+    assert datetime_obj.day == 28
+    assert datetime_obj.hour == 15
+    assert datetime_obj.minute == 53
+    assert datetime_obj.second == 0
