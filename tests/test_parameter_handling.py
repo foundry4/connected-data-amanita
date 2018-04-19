4bb6f1194874e120cb6d40dd49c8b8ed64d76c44
windet01 <theo.windebank@bbc.co.uk>
2018-04-19 11:31:40 +0100

    tests for parameter validator

--- tests/test_parameter_handling.py
+++ tests/test_parameter_handling.py
@@ -3,6 +3,8 @@ from rdflib import URIRef
 
 from app.apiparams import lists, types, validator
 from app.apiparams.types import BoolFromString, MediaLiteral, LowercaseLiteral, ValidatedDatetime
+from app.apiparams.validator import ParamValidator
+from exceptions.queryexceptions import InvalidInputParameterValue
 
 
 class Definitions:
@@ -72,3 +74,70 @@ def test_validated_datetime():
     assert datetime_obj.hour == 15
     assert datetime_obj.minute == 53
     assert datetime_obj.second == 0
+
+
+# test validator class
+def test_param_validator():
+    class TestValue:
+        def __init__(self, value):
+            if value == 'correct':
+                self.val = value
+            else:
+                raise ValueError('Incorrect input.')
+
+    class TestAllowedValues:
+        def __init__(self, value):
+            self.val = value
+
+    # test single correctly formatted
+    validator = ParamValidator('test_param', TestValue)
+    validated_param = validator.validate('correct')
+    assert isinstance(validated_param, TestValue)
+    assert validated_param.val == 'correct'
+    # test incorrectly formatted value
+    with pytest.raises(ValueError):
+        validator.validate('incorrect')
+    # test allowed value
+    validator = ParamValidator('test_param', TestAllowedValues, allowed_values=['allowed'])
+    validated_param = validator.validate('allowed')
+    assert isinstance(validated_param, TestAllowedValues)
+    assert validated_param.val == 'allowed'
+    # test not allowed value
+    with pytest.raises(InvalidInputParameterValue):
+        validator.validate('notallowed')
+    # test list of correctly formatted values (single value)
+    validator = ParamValidator('test_param', TestValue, is_list=True)
+    validated_param = validator.validate('correct')
+    assert isinstance(validated_param, list)
+    # test list of incorrectly formatted values (single value)
+    with pytest.raises(ValueError):
+        validator.validate('incorrect')
+    # test list of correctly formatted values (multiple values)
+    validated_param = validator.validate(['correct', 'correct'])
+    assert isinstance(validated_param, list)
+    assert all(isinstance(param, TestValue) for param in validated_param)
+    assert all(param.val == 'correct' for param in validated_param)
+    # test list of incorrectly formatted values (multiple values)
+    with pytest.raises(ValueError):
+        validator.validate(['incorrect','incorrect'])
+    # test list of mixed formatted values (multiple values)
+    with pytest.raises(ValueError):
+        validator.validate(['correct', 'incorrect'])
+    # test list of allowed values
+    validator = ParamValidator('test_param', TestAllowedValues, allowed_values=['allowed'], is_list=True)
+    validated_param = validator.validate('allowed')
+    assert isinstance(validated_param, list)
+    # test list of non allowed values (single value)
+    with pytest.raises(InvalidInputParameterValue):
+        validator.validate('notallowed')
+    # test list of allowed values (multiple values)
+    validated_param = validator.validate(['allowed', 'allowed'])
+    assert isinstance(validated_param, list)
+    assert all(isinstance(param, TestAllowedValues) for param in validated_param)
+    assert all(param.val == 'allowed' for param in validated_param)
+    # test list of not allowed values (multiple values)
+    with pytest.raises(InvalidInputParameterValue):
+        validator.validate(['notallowed', 'notallowed'])
+    # test list of mixed allowed values (multiple values)
+    with pytest.raises(InvalidInputParameterValue):
+        validator.validate(['allowed', 'notallowed'])
